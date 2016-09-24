5779caaba8e474aa5649e427489dd6fb834dacab
Brice Huang <bmhuang@mit.edu>
2016-09-24 00:00:18 -0400

    ayy lmao this actually works

--- ps1/code/P1/gradientDescent.m
+++ ps1/code/P1/gradientDescent.m
@@ -3,6 +3,10 @@ function gradientDescent()
 function w = gradientDescent_1(fn, grad, startPoint, stepSize, convergenceThreshold)
 currentPoint = startPoint;
 while (norm(grad(currentPoint)) > convergenceThreshold)
+    disp(111111111111111111111);
+    disp(currentPoint);
+    disp(norm(currentPoint));
+    disp(grad(norm(currentPoint)));
     currentPoint = currentPoint - stepSize*grad(currentPoint);
 end
 w = currentPoint;
@@ -25,13 +29,14 @@ v = @(x) out(x);
 end
 
 [gaussMean, gaussCov, quadBowlA, quadBowlb] = loadParametersP1();
-myfn = mymvnpdf(gaussMean, gaussCov);
+%myfn = mymvnpdf(gaussMean, gaussCov);
 myfngrad = mymvnpdfgrad(gaussMean, gaussCov);
-disp(mvnpdf([9.9; 10.1], gaussMean, gaussCov));
-disp(myfn([9.9; 10.1]));
-startPoint = [9.9; 10.1];
-stepSize = 0.00001;
-convergenceThreshold = 0.00001;
-v = gradientDescent_1(mymvnpdf(gaussMean, gaussCov), mymvnpdfgrad(gaussMean, gaussCov), startPoint, stepSize, convergenceThreshold)
+%disp(mvnpdf([9.9; 10.1], gaussMean, gaussCov));
+disp(myfngrad([40; 40]));
+startPoint = [1; 19];
+stepSize = 1000000;
+convergenceThreshold = 1.0e-10;
+v = gradientDescent_1(mymvnpdf(gaussMean, gaussCov), mymvnpdfgrad(gaussMean, gaussCov), startPoint, stepSize, convergenceThreshold);
+disp(v);
 
 end
