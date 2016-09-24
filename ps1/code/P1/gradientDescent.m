3a0732ab89d0f155fe6c8cf75e2a06063b2d1bd7
Brice Huang <bmhuang@mit.edu>
2016-09-24 18:06:56 -0400

    Added documentation to gradientDescent, implemented approx gradient

--- ps1/code/P1/gradientDescent.m
+++ ps1/code/P1/gradientDescent.m
@@ -1,5 +1,13 @@
 function gradientDescent()
 
+%Finds the x for which fn(x) is minimal by gradient descent
+% fn: the (scalar) function to be minimized
+% grad: a (vector) function, equal to the gradient of fn or an approximation
+% startPoint: starting point for gradient descent
+% step size: learning rate, so each step is stepSize * gradient(point)
+% convergenceThreshold: determines when algorithm stops.  We terminate when
+% |grad(point)| < convergenceThreshold
+% return: point where gradient descent terminated
 function w = gradientDescent_1(fn, grad, startPoint, stepSize, convergenceThreshold)
 currentPoint = startPoint;
 while (norm(grad(currentPoint)) > convergenceThreshold)
@@ -12,6 +20,10 @@ end
 w = currentPoint;
 end
 
+% outputs a multivariate gaussian distribution
+% mean: mean vector
+% cov: covariance matrix
+% return: a function f such that f(x) is Gaussian(mean, cov)(x)
 function v = mymvnpdf(mean, cov)
 n = size(cov, 1);
     function w = out(x)
@@ -20,6 +32,10 @@ n = size(cov, 1);
 v = @(x) out(x);
 end
 
+% outputs a multivariate gaussian distribution's gradient
+% mean: mean vector of multivariate gaussian 
+% cov: covariance matrix of multivariate gaussian
+% return: a function f such that f(x) is gradient(Gaussian(mean, cov))(x)
 function v = mymvnpdfgrad(mean, cov)
     function w = out(x)
         f = mymvnpdf(mean, cov);
@@ -28,15 +44,35 @@ function v = mymvnpdfgrad(mean, cov)
 v = @(x) out(x);
 end
 
+% outputs an approximation of a function's gradient
+% fn: function whose gradient is to be computed
+% epsilon: half-width of finite difference used to compute gradient
+% return: a function f such that f(x) approximates gradient(fn)(x)
+function v = approxGradient(fn, epsilon)
+    function w = out(x)
+        n = length(x);
+        grad = zeros(size(x));
+        for k = 1:n
+            smallvector = zeros(size(x));
+            smallvector(k) = smallvector(k) + epsilon;
+            grad(k) = (fn(x+smallvector)-fn(x-smallvector)) / (2*epsilon);
+        end
+        w = grad;
+    end
+v = @(x) out(x);
+end
+
 [gaussMean, gaussCov, quadBowlA, quadBowlb] = loadParametersP1();
-%myfn = mymvnpdf(gaussMean, gaussCov);
+myfn = mymvnpdf(gaussMean, gaussCov);
 myfngrad = mymvnpdfgrad(gaussMean, gaussCov);
+myfnapproxgrad = approxGradient(myfn,0.1);
 %disp(mvnpdf([9.9; 10.1], gaussMean, gaussCov));
 disp(myfngrad([40; 40]));
-startPoint = [1; 19];
-stepSize = 1000000;
-convergenceThreshold = 1.0e-10;
-v = gradientDescent_1(mymvnpdf(gaussMean, gaussCov), mymvnpdfgrad(gaussMean, gaussCov), startPoint, stepSize, convergenceThreshold);
-disp(v);
+disp(myfnapproxgrad([40; 40]));
+%startPoint = [1; 19];
+%stepSize = 1000000;
+%convergenceThreshold = 1.0e-12;
+%v = gradientDescent_1(mymvnpdf(gaussMean, gaussCov), mymvnpdfgrad(gaussMean, gaussCov), startPoint, stepSize, convergenceThreshold);
+%disp(v);
 
 end
