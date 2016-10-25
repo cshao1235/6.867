2862f3b1201c65f28f69ce0fbc200688f23779f2
Christopher Shao <cshao@mit.edu>
2016-10-24 21:54:40 -0400

    Implemented svm_test

--- ps2/hw2_resources/svm_test.m
+++ ps2/hw2_resources/svm_test.m
@@ -6,20 +6,18 @@ X = data(:,1:2);
 Y = data(:,3);
 
 % Carry out training, primal and/or dual
-function sol = dualSVM(X, Y, C)
-    n = size(Y, 2);
+function sol = dualSVM(X, Y, C, kernel)
+    n = size(Y, 1);
     H = zeros(n, n);
     for i = 1:n
         for j = 1:n
-            H(i, j) = Y(i)*Y(j)*dot(X(i),X(j));
+            H(i, j) = Y(i)*Y(j)*kernel(X(i,:),X(j,:));
         end
     end
-    f = zeros(1, n);
+    f = zeros(n, 1);
     for i = 1:n
-        f(i) = -1;
+        f(i, 1) = -1;
     end
-    A = zeros(1, n);
-    b = zeros(1, 1);
     Aeq = zeros(1, n);
     for i = 1:n
         Aeq(i) = Y(i);
@@ -28,34 +26,50 @@ function sol = dualSVM(X, Y, C)
     lb = zeros(n, 1);
     ub = zeros(n, 1);
     for i = 1:n
-        ub(i) = C;
+        ub(i, 1) = C;
     end
-%     optim_ver = ver('optim');
-%     disp(optim_ver);
-%     optim_ver = str2double(optim_ver.Version);
-%     if optim_ver >= 6
-%         opts = optimset('Algorithm', 'interior-point-convex');
-%     else
+    optim_ver = ver('optim');
+    optim_ver = str2double(optim_ver.Version);
+    if optim_ver >= 6
+        opts = optimset('Algorithm', 'interior-point-convex');
+    else
     opts = optimset('Algorithm', 'interior-point', 'LargeScale', 'off', 'MaxIter', 2000);
-%     end
-    sol = quadprog(H, f, A, b, Aeq, beq, lb, ub, [], opts);
+    end
+    sol = quadprog(H, f, [], [], Aeq, beq, lb, [], [], opts);
 end
 
-trainedParameters = dualSVM(X, Y, 10000);
+function z = kernel(x, y)
+    z = dot(x, y);
+end
+
+C = 1;
+trainedParameters = dualSVM(X, Y, C, @(x,y) kernel(x, y));
+d = size(X, 2);
+n = size(X, 1);
+w = zeros(d, 1);
+for i = 1:n
+    if(trainedParameters(i) ~= 0)
+        w = w + trainedParameters(i)*Y(i)*X(i, :)';
+    end
+end
+nonzeroCount = 0;
+b = 0;
+for i = 1:n
+    if (trainedParameters(i) > 0)
+        b = b + Y(i);
+        for j = 1:n
+            if (trainedParameters(j) > 0)
+                b = b - trainedParameters(j)*Y(j)*kernel(X(i,:), X(j,:));
+            end
+        end
+        nonzeroCount = nonzeroCount + 1;
+    end
+end
+b = b/nonzeroCount;
+disp(norm(w));
 
 % Define the predictSVM(x) function, which uses trained parameters
 function y = predictSVM()
-    d = size(X, 1);
-    n = size(X, 2);
-    w = zeros(d);
-    for i = 1:n
-        w = w + trainedParameters(i)*X(i);
-    end
-    for i = 1:n
-        if (trainedParameters(i) < C && trainedParameters(i) > 0)
-            b = 1.0/Y(i) - dot(w, X(i));
-        end
-    end
     y = @(x) (dot(w, x) + b);
 end
 
