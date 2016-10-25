f3b84259f2e8c539948c09be143b255919b95428
Christopher Shao <cshao@mit.edu>
2016-10-25 00:04:21 -0400

    Implemented pegasos with gaussian kernel

--- ps2/hw2_resources/pegasos_gaussian_test.m
+++ ps2/hw2_resources/pegasos_gaussian_test.m
@@ -15,32 +15,36 @@ global gamma
 global K
 epochs = 1000;
 lambda = .02;
-gamma = 2^2;
+gamma = 2^1;
+
+function z = gaussianKernel(x1, x2)
+    z = exp(-1*(norm(x1 - x2)*norm(x1 - x2))/gamma);
+end
 
 K = zeros(n,n);
 %%% TODO: Compute the kernel matrix %%%
 for i = 1:n
     for j = 1:n
-        K(i, j) = exp(-1*gamma*(norm(X(i,:), X(j,:))*norm(X(i,:), X(j,:))));
+        K(i, j) = gaussianKernel(X(i,:), X(j,:));
     end
 end
 %%% TODO: Implement train_gaussianSVM %%%
 function train_gaussianSVM(X, Y, lambda, epochs)
     t = 0;
-    alphas = zeros(size(X, 2), 1);
+    alpha = zeros(size(X, 1), 1);
     epoch = 0;
     while (epoch < epochs)
         for i = 1:size(X, 1)
             t = t + 1;
-            eta = 1/(t*lambda)
+            eta = 1/(t*lambda);
             sum = 0;
             for j = 1:size(X, 1)
-                sum = sum + alphas(j, 1)*K(j, i);
+                sum = sum + alpha(j, 1)*K(j, i);
             end
             if(Y(i)*sum < 1)
-                alphas(i) = (1 - eta*lambda)*alphas(i) + eta*Y(i);
+                alpha(i) = (1 - eta*lambda)*alpha(i) + eta*Y(i);
             else
-                alphas(i) = (1 - eta*lambda)*alphas(i)
+                alpha(i) = (1 - eta*lambda)*alpha(i);
             end
         end
         epoch = epoch + 1;
@@ -52,12 +56,18 @@ train_gaussianSVM(X, Y, lambda, epochs);
 
 % Define the predict_gaussianSVM(x) function, which uses trained parameters, alpha
 function z = predict_gaussianSVM(x)
-    z = dot(
+    sum = 0;
+    for i = 1:size(X, 1)
+        sum = sum + alpha(i)*gaussianKernel(X(i,:), x);
+    end
+    z = sum;
 end
 
 hold on;
 
 % plot training results
 plotDecisionBoundary(X, Y, @predict_gaussianSVM, [-1,0,1], 'Gaussian Kernel SVM');
+hold off;
+end
 
 
